3bcef390b16fe1a0b0d499aa1a9e4851bc539fc9
nana-k-0310 <nana-7591-ai-1638@outlook.jp>
2023-02-28 12:28:58 +0900

    user情報を操作するサービス

---
+++ src/main/java/com/example/service/ResisterUserService.java
@@ -0,0 +1,31 @@
+package com.example.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.User;
+import com.example.repository.UserRepository;
+
+/**
+ * ユーザー情報を操作するサービス.
+ * 
+ * @author nanakono
+ *
+ */
+@Service
+@Transactional
+public class ResisterUserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	/**
+	 * ユーザー情報を登録します.
+	 * 
+	 * @param user　ユーザー情報
+	 */
+	public void resisterUser(User user) {
+		userRepository.save(user);
+	}
+}
